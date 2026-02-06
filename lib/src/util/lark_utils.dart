8281281bae839fa2fe60ee2ba6e33677c5d86731
Howard Wang <sunbird.work@gmail.com>
2026-02-06 18:13:28 +0800

    add tunnel support

---
+++ lib/src/util/lark_utils.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'dart:io';
+
+const String url =
+    "https://open.feishu.cn/open-apis/bot/v2/hook/40d8befe-4f22-4baf-a97d-3d94b0f12406";
+
+class LarkUtils {
+  static Future<bool> sendMessage(String message) async {
+    final body = {
+      "msg_type": "text",
+      "content": {"text": message}
+    };
+    final HttpClient client = HttpClient();
+    final request = await client.postUrl(Uri.parse(url));
+    request.headers.set("Content-Type", "application/json");
+    request.write(jsonEncode(body));
+    final response = await request.close();
+    return true;
+  }
+}
