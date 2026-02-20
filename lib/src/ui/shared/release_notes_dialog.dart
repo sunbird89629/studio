dcc1e2b70060093ca92a52e73b9b16079ddb69fa
Howard Wang <sunbird.work@gmail.com>
2026-02-20 23:29:50 +0800

    upgrade dependencies

--- lib/src/ui/shared/release_notes_dialog.dart
+++ lib/src/ui/shared/release_notes_dialog.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:terminal_studio/src/core/service/release_notes_service.dart';
 
